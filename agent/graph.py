c29a4a83727daef1954c3a66e4b2fae3462e133f
Deepali <deepalilanjewar07@gmail.com>
2026-04-25 21:42:35 +0530

    Initial commit

---
+++ agent/graph.py
@@ -0,0 +1,7 @@
+from dotenv import load_dotenv
+load_dotenv()
+from langchain_groq import ChatGroq
+
+llm = ChatGroq(model="openai/gpt-oss-120b")
+resp=(llm.invoke("Who invented Kriya yoga. answer in one sentence"))
+print(resp.content)
